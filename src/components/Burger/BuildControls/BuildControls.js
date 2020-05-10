39b8fe6686026825110cc36f46126e86340b3a9e
Calvin Christian <calvinchrst@gmail.com>
2020-05-10 21:07:18 +0800

    Creating Build Controls and Build Control Components (UI only)

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import * as BI from "../BurgerIngredient/BurgerIngredient";
+import util from "../../../util/util";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  {
+    label: util.stringToSentenceCase(BI.INGREDIENT_SALAD),
+    type: BI.INGREDIENT_SALAD,
+  },
+  {
+    label: util.stringToSentenceCase(BI.INGREDIENT_BACON),
+    type: BI.INGREDIENT_BACON,
+  },
+  {
+    label: util.stringToSentenceCase(BI.INGREDIENT_CHEESE),
+    type: BI.INGREDIENT_CHEESE,
+  },
+  {
+    label: util.stringToSentenceCase(BI.INGREDIENT_MEAT),
+    type: BI.INGREDIENT_MEAT,
+  },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
+  </div>
+);
+
+export default buildControls;
