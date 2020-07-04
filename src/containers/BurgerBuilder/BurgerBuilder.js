c19f53e82dc2aabea9889c9ada6ab99335436bad
Calvin Christian <calvinchrst@gmail.com>
2020-07-04 16:36:10 +0800

    Get ingredients list from server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,16 +19,24 @@ const INGREDIENTS_PRICE = {
 
 class BurgerBuilder extends Component {
   state = {
-    ingredients: {
-      salad: 0, // TODO: Somehow change this to use Constant declared in BurgerIngredient.js
-      bacon: 0,
-      cheese: 0,
-      meat: 0,
-    },
+    ingredients: null,
     totalPrice: 4,
     purchasable: false,
     purchasing: false,
     loading: false,
+    error: false,
+  };
+
+  componentDidMount = () => {
+    axios
+      .get("/ingredients.json")
+      .then((result) => {
+        console.log(result.data);
+        this.setState({ ingredients: result.data });
+      })
+      .catch((err) => {
+        this.setState({ error: true });
+      });
   };
 
   updatePurchasableState = (ingredients) => {
@@ -106,26 +114,51 @@ class BurgerBuilder extends Component {
   };
 
   render() {
-    // Disable less button if we don't have ingredients of some type
-    // DisabledInfo is an object that contains
-    //    key: ingredient name and
-    //    value: boolean true / false if the ingredient less button should be disabled
-    const disabledInfo = { ...this.state.ingredients };
-    for (let ingredient in disabledInfo) {
-      disabledInfo[ingredient] = disabledInfo[ingredient] <= 0;
+    let burger = <Spinner />;
+    let orderSummary = null;
+
+    if (this.state.ingredients) {
+      // Disable less button if we don't have ingredients of some type
+      // DisabledInfo is an object that contains
+      //    key: ingredient name and
+      //    value: boolean true / false if the ingredient less button should be disabled
+      const disabledInfo = { ...this.state.ingredients };
+      for (let ingredient in disabledInfo) {
+        disabledInfo[ingredient] = disabledInfo[ingredient] <= 0;
+      }
+
+      burger = (
+        <Auxiliary>
+          <Burger ingredients={this.state.ingredients} />
+          <BuildControls
+            addIngredient={this.addIngredientHandler}
+            removeIngredient={this.removeIngredientHandler}
+            disabledInfo={disabledInfo}
+            purchasable={this.state.purchasable}
+            price={this.state.totalPrice}
+            purchasing={this.purchasingHandler}
+          />
+        </Auxiliary>
+      );
+
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.state.ingredients}
+          purchaseCancelled={this.purchasingCancelHandler}
+          purchaseContinued={this.purchasingContinueHandler}
+          price={this.state.totalPrice}
+        />
+      );
     }
 
-    let orderSummary = (
-      <OrderSummary
-        ingredients={this.state.ingredients}
-        purchaseCancelled={this.purchasingCancelHandler}
-        purchaseContinued={this.purchasingContinueHandler}
-        price={this.state.totalPrice}
-      />
-    );
     if (this.state.loading) {
       orderSummary = <Spinner />;
     }
+
+    if (this.state.error) {
+      burger = <p>Can't fetch ingredients from server</p>;
+    }
+
     return (
       <Auxiliary>
         <Modal
@@ -134,15 +167,7 @@ class BurgerBuilder extends Component {
         >
           {orderSummary}
         </Modal>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls
-          addIngredient={this.addIngredientHandler}
-          removeIngredient={this.removeIngredientHandler}
-          disabledInfo={disabledInfo}
-          purchasable={this.state.purchasable}
-          price={this.state.totalPrice}
-          purchasing={this.purchasingHandler}
-        />
+        {burger}
       </Auxiliary>
     );
   }
