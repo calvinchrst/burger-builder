1b9e36f3199eb2ac537d2a740a434600731e8eb6
Calvin Christian <calvinchrst@gmail.com>
2020-07-17 17:54:02 +0800

    Make order submission works.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,12 +6,8 @@ import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1,
-    },
+    ingredients: null,
+    price: 0,
   };
 
   checkoutCancelledHandler = () => {
@@ -22,14 +18,19 @@ class Checkout extends Component {
     this.props.history.replace("/checkout/contact-data");
   };
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price;
     for (let param of query.entries()) {
-      // ["Salad", "1"]
-      ingredients[param[0]] = +param[1];
+      if (param[0] === "price") {
+        price = param[1];
+      } else {
+        // ["Salad", "1"]
+        ingredients[param[0]] = +param[1];
+      }
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({ ingredients: ingredients, price: price });
   }
 
   render() {
@@ -42,7 +43,13 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + "/contact-data"}
-          component={ContactData}
+          render={(props) => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.price}
+              {...props}
+            />
+          )}
         />
       </div>
     );
