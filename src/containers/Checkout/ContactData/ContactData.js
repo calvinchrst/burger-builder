aaaf9bb63a807b2beff221be567bf7abecb72961
Calvin Christian <calvinchrst@gmail.com>
2020-08-20 20:26:03 +0800

    Fix default value of delivery method dropdown to non-empty.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,7 +85,7 @@ class ContactData extends Component {
             { value: "cheapest", displayValue: "Cheapest" },
           ],
         },
-        value: "",
+        value: "fastest",
         validation: {},
         valid: true,
       },
