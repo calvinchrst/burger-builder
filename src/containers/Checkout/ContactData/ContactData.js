862c87a6571689b83ac7ae3fee994327a1562950
Calvin Christian <calvinchrst@gmail.com>
2020-08-20 20:24:46 +0800

    Fix bug where changing delivery method in contact form causes error.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
           ],
         },
         value: "",
+        validation: {},
         valid: true,
       },
     },
@@ -143,6 +144,10 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
