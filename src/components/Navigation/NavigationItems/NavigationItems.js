dc77eaf6bff41bd5c299aebe6e8f495b172981aa
Calvin Christian <calvinchrst@gmail.com>
2020-08-27 20:55:49 +0800

    Add Authenticate page with email and password form (Form can't submit yet).

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,6 +10,7 @@ const navigationItems = () => {
         Burger Builder
       </NavigationItem>
       <NavigationItem link="/orders">Orders</NavigationItem>
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
   );
 };
