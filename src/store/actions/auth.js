67d773a593fe669dbab1dfbd373f1f7930b871c5
Calvin Christian <calvinchrst@gmail.com>
2020-08-28 13:03:53 +0800

    Implement sign up part of the auth form that connects to firebase.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
+import * as config from "../../config/default.json";
 
 const authSuccess = (authData) => {
   return {
@@ -23,5 +25,19 @@ const authStart = () => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(config.FIREBASE_SIGN_UP_URI + config.FIREBASE_WEB_API_KEY, authData)
+      .then((res) => {
+        dispatch(authSuccess(res.data));
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch(authFail(error));
+      });
   };
 };
