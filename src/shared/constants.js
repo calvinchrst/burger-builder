041b61db33ed7690ef8b627966484852c07a2932
Calvin Christian <calvinchrst@gmail.com>
2020-09-01 13:59:10 +0800

    Only enable redux dev tools in development mode.

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,3 +1,4 @@
 export const STRING_TOKEN = "TOKEN";
 export const STRING_EXPIRATION_DATE = "EXPIRATION_DATE";
 export const STRING_USER_ID = "USER_ID";
+export const STRING_DEVELOPMENT = "development";
